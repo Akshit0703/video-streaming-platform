f61b8888d262d2f3cf8a277f29fb753671c692f3
Akshit0703 <akshit.lakhanpal07@gmail.com>
2026-01-09 00:02:19 +0530

    user regosteration

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,8 @@
 import { User } from "../models/user.js";
 import { ApiError } from "../utils/apiError.js";
 import asyncHandler from "../utils/asyncHandler.js";
+import uploadOnCloudinary from "../utils/cloudinary.js";
+import ApiResponse from "../utils/apiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   const { fullname, email, username, password } = req.body;
@@ -20,6 +22,40 @@ const registerUser = asyncHandler(async (req, res) => {
   if (existedUser) {
     throw new ApiError(409, "User with email or username already exists");
   }
+  const avatarLocalPath = req.files?.avatar[0]?.path; // this is possible due to using multer in the middleware
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar is required");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath); //returns cloudinary url
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath); //return cloudinary url
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar is required");
+  }
+
+  const User = User.create({
+    fullname,
+    avatar: avatar.url,
+    coverImage: coverImage.url || "", // edge case if no coverimage is given since it is not required in the db
+    email: email,
+    password: password,
+    username: username.toLowerCase(), // for consistency
+  });
+
+  const createdUser = await User.findOne(User._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+
+  return status(201).json(
+    new ApiError(200, createdUser, "User registered successfully")
+  );
 });
 
 export { registerUser };
